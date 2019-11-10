22368aaa2b7b67de30f8ef4c93b7515a736f50fd
SentientSam <samueltlamb1997@gmail.com>
2019-11-10 10:34:44 -0600

    Credential File Update

--- GUI10_Credentials.py
+++ GUI10_Credentials.py
@@ -12,29 +12,43 @@ def write_File_3 (total):
     file = open("Note.txt", "a")
     file.write('Password: ' + total.get() + '\n')
     file.close()
+def write_File_4 (total):
+    file = open("Note.txt","a")
+    file.write('Add Info: ' + total.get() + '\n')
+    file.close() #added in an additional info section just incase there are security codes, hints, etc.
 
 window = Tk()
+
 #see label & grid tutorials if you need help with the formatting below
+
 service = Label(window,text="Service:")
 service.grid(row=1, column=1)
 username = Label(window,text="Username:")
 username.grid(row=2, column=1)
 password = Label(window,text="Password:")
 password.grid(row=3, column=1)
-
+addInfo = Label(window,text="Additonal Info:")
+addInfo.grid(row=4, column=1)
+#grid just helps to make things looks smooth for these files
 input_1 = Entry(window)
 input_1.grid(row=1, column=2)
 input_2 = Entry(window)
 input_2.grid(row=2, column=2)
 input_3 = Entry(window)
 input_3.grid(row=3, column=2)
+input_4 = Entry(window)
+input_4.grid(row=4, column=2)
 
-button_1 = Button( window , text = "Send to Note.txt" , command = lambda:[write_File_1(input_1),write_File_2(input_2),write_File_3(input_3)]).grid(row=4, columnspan=2, column=1)
+
 #lambda is a service that allows for multiple functions to be assigned to one widget. I called all the functions I build to activate with the click of button_1
+button_1 = Button( window , text = "Send to Note.txt" , command = lambda:[write_File_1(input_1),write_File_2(input_2),write_File_3(input_3),write_File_4(input_4)]).grid(row=5, columnspan=2, column=1)
 
-# Output would be: Service: <input_3>
+
+
+# Output would be:  Service: <input_3>
 #                  Username: <input_2>
 #                  Password: <input_3>
+#                  Add Info: <input_4>
 
 
 
